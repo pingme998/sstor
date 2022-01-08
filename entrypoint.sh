73476935d41fd4c404e82dfdbb0aefae782f6bd1
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-08 09:50:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,6 +89,6 @@ else
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/vpn.png
 fi
 
-ss-server -c /etc/shadowsocks-libev/config.json &
+proxychains4 ss-server -c /etc/shadowsocks-libev/config.json &
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
